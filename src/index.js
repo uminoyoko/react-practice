cb5d358d76384e94e9ddced1298b07532e0130b5
Mio <axbu.le.paco126@gmail.com>
2020-10-18 17:51:00 +0900

    new

--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { findAllByPlaceholderText } from '@testing-library/react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Part3
+// const Display = ({counter}) =><div>{counter}</div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const Button = ({handleClick,text}) =>{
+//   return(
+//     <button onClick={handleClick}>
+//       {text}
+//     </button>
+//   )
+// }
+
+// const App = () =>{
+//   const [counter,setCounter]=useState(0)
+//   const increaseByOne = ()=> setCounter(counter+1)
+//   const decreaseByOne = ()=> setCounter(counter-1)
+//   const setToZero = ()=> setCounter(0)
+
+//   return(
+//     <div>
+//       <Display counter={counter}/>
+//       <Button
+//         handleClick={increaseByOne}
+//         text='plus'
+//       />
+//       <Button 
+//         handleClick={setToZero}
+//         text='zero'
+//       />
+//       <Button
+//         handleClick={decreaseByOne}
+//         text='minus'
+//       />
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+const History=(props)=>{
+  if(props.allCkicks.length===0){
+    return(
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return(
+    <div>
+      button press history:{props.allCkicks.join('')}
+    </div>
+  )
+}
+
+const Button=({onClick,text})=>(
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = (props) =>{
+  const[left,setLeft]=useState(0)
+  const[right,setRight]=useState(0)
+  const[allCkicks,setAll]=useState([])
+
+  const handleLeftClick=()=>{
+    setAll(allCkicks.concat('L'))
+    setLeft(left+1)
+  }
+
+  const handleRightClick=()=>{
+    setAll(allCkicks.concat('R'))
+    setRight(right+1)
+  }
+
+  return(
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text='left'/>
+        <Button onClick={handleRightClick} text='right'/>
+        {right}
+        <History allCkicks={allCkicks}/>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
